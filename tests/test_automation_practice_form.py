1bd6bf6754771d7eb3d3810673ffca98dca60f14
MarySukhorukova <57411226+MarySukhorukova@users.noreply.github.com>
2022-12-15 06:40:08 +0300

    задание selene

--- tests/test_automation_practice_form.py
+++ tests/test_automation_practice_form.py
@@ -1,3 +1,5 @@
+import time
+
 from selene import be, have
 from selene.support.shared import browser
 import os
@@ -16,14 +18,20 @@ def test_filling_and_submitting_form():
     browser.element('.react-datepicker__year-select').click()
     browser.element('[value="1980"]').click()
     browser.element('.react-datepicker__day--031').click()
-    browser.element('#subjectsInput').type('Dark Arts')
+    browser.element('#subjectsInput').type('Arts').press_enter()
     browser.element('[for="hobbies-checkbox-1"]').click()
+    time.sleep(5)
     browser.element('#uploadPicture').set_value(os.path.abspath('files/Pytest_logo.svg.png'))
+    time.sleep(5)
     browser.element('#currentAddress').set_value('Hogwarts').press_enter()
+    time.sleep(5)
     browser.element('#react-select-3-input').send_keys('Haryana').press_enter()
+    time.sleep(5)
     browser.element('#react-select-4-input').send_keys('Karnal').press_enter()
+    time.sleep(5)
     browser.element('#submit').press_enter()
-
+    time.sleep(5)
     browser.element('#example-modal-sizes-title-lg').should(have.text('Thanks for submitting the form'))
+    time.sleep(5)
     browser.element('.table').should(have.text('Harry Potter' and 'hp@test.com' and 'Male' and '0123456789' and '31 July,1980'
-                                               and 'Dark Arts' and 'Sports' and 'Pytest_logo.svg.png' and 'Hogwarts'))
+                                               and 'Arts' and 'Sports' and 'Pytest_logo.svg.png' and 'Hogwarts'))
